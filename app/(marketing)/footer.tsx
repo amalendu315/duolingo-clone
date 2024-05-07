0d546785b7e301a74abe66f7f350e092a8bbba10
Amalendu Pandey <amalendupandey56@gmail.com>
2024-05-07 14:35:47 +0530

    Layout Completed

---
+++ app/(marketing)/footer.tsx
@@ -0,0 +1,61 @@
+import { Button } from "@/components/ui/button"
+import Image from "next/image"
+
+export const Footer = () => {
+    return (
+      <footer className="hidden lg:block h-20 w-full border-t-2 border-slate-200 p-2">
+        <div className="max-w-screen-lg mx-auto flex items-center justify-evenly h-full">
+          <Button size={"lg"} variant="ghost" className="w-full">
+            <Image
+              src="/assets/croatian.svg"
+              alt="Croatian"
+              height={32}
+              width={40}
+              className="mr-4 rounded-md"
+            />
+            Croatian
+          </Button>
+          <Button size={"lg"} variant="ghost" className="w-full">
+            <Image
+              src="/assets/spanish.svg"
+              alt="Spanish"
+              height={32}
+              width={40}
+              className="mr-4 rounded-md"
+            />
+            Spanish
+          </Button>
+          <Button size={"lg"} variant="ghost" className="w-full">
+            <Image
+              src="/assets/english.svg"
+              alt="English"
+              height={32}
+              width={40}
+              className="mr-4 rounded-md"
+            />
+            English
+          </Button>
+          <Button size={"lg"} variant="ghost" className="w-full">
+            <Image
+              src="/assets/german.svg"
+              alt="German"
+              height={32}
+              width={40}
+              className="mr-4 rounded-md"
+            />
+            German
+          </Button>
+          <Button size={"lg"} variant="ghost" className="w-full">
+            <Image
+              src="/assets/italian.svg"
+              alt="Italian"
+              height={32}
+              width={40}
+              className="mr-4 rounded-md"
+            />
+            Italian
+          </Button>
+        </div>
+      </footer>
+    );
+}
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import { Loader } from "lucide-react";
+import { ClerkLoaded, ClerkLoading, SignInButton, SignUpButton, SignedIn, SignedOut } from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+
+export default function Home() {
+  return (
+    <div className="max-w-[988px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p-4 gap-2">
+      <div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
+        <Image src="/assets/hero.svg" fill alt="hero" />
+      </div>
+      <div className="flex flex-col items-center gap-y-8">
+        <h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+          Learn, practice and master new languages with{" "}
+          <span className="text-green-600">duolingo</span>.
+        </h1>
+        <div className="flex flex-col items-center gap-x-3 max-w-[330px] w-full">
+          <ClerkLoading>
+            <Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedOut>
+              <SignUpButton
+                mode="modal"
+                forceRedirectUrl="/learn"
+                signInForceRedirectUrl="/learn"
+              >
+                <Button className="w-full" size="lg" variant="secondary">
+                  Get Started
+                </Button>
+              </SignUpButton>
+              <SignInButton
+                mode="modal"
+                forceRedirectUrl="/learn"
+                signUpForceRedirectUrl="/learn"
+              >
+                <Button className="w-full mt-2" size="lg" variant="primaryOutline">
+                  I already have an account
+                </Button>
+              </SignInButton>
+            </SignedOut>
+            <SignedIn>
+              <Button size={"lg"} variant={"secondary"} className="w-full" asChild>
+                <Link href={"/learn"}>Continue Learning</Link>
+              </Button>
+            </SignedIn>
+          </ClerkLoaded>
+        </div>
+      </div>
+    </div>
+  );
+}
