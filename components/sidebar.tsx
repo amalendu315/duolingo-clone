e3b5d07ce584974ce078b11e89610c0f74edd06b
Amalendu Pandey <amalendupandey56@gmail.com>
2024-05-11 20:22:23 +0530

    Created the lesson page

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -19,7 +19,7 @@ export const Sidebar = ({ className }: Props) => {
         className
       )}
     >
-      <Link href={"/learn"}>
+      <Link href={"/"}>
         <div className="pt-8 pl-4 pb-7 flex items-center gap-x-3">
           <Image src={"/assets/logo.svg"} height={40} width={40} alt="Mascot" />
           <h1 className="text-2xl font-extrabold text-green-600 tracking-wide">
